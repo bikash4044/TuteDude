8268b67800d606282d295ee29458b5e9519113f0
Bikash Ranjan Behera <90145679+bikash4044@users.noreply.github.com>
2025-11-13 13:23:53 +0530

    upto assignment2

---
+++ Python/Assignment1_task1.py
@@ -0,0 +1,12 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
---
+++ Python/Assignment1_task2.py
@@ -0,0 +1,6 @@
+f_name = input("Enter first name: ")
+l_name = input("Enter last name: ")
+
+name = f_name + " " + l_name
+
+print(f"Hello, {name}! Welcome to the Python program.")

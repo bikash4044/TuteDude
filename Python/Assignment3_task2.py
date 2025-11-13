dce360c8fad7c2b4a016d43d4abde978ee28fb56
Bikash Ranjan Behera <90145679+bikash4044@users.noreply.github.com>
2025-11-13 14:07:07 +0530

    upto python 3

---
+++ Python/Assignment3_task2.py
@@ -0,0 +1,5 @@
+import math
+num = float(input("Enter a number: "))
+print(f"Square root of {num} is: ", math.sqrt(num))
+print(f"Logarithm of {num} is: ", math.log(num))
+print(f"Sine of {num} is: ", math.sin(num))
